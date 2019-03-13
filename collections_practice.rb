85fb12e93f8a5dd86996f5ce4d427b314df3fa7f
Darcy Linde <lindedarcy@gmail.com>
2019-03-13 13:39:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,8 @@ end
 def merge_data(keys, data)
  new_array = []
  keys.each_with_index do |val, index|
-   new_array << val.merge(data[0][])
+   name = val[index][:first_name]
+   new_array << val.merge(data[0][name])
  end
  new_array
 end
